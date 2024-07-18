1939036d04462ff3e4119a5568530f6d3f39dac9
Nicolas Corbett <nicolas.corbett@student.ehb.be>
2024-07-18 14:57:09 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css';
+import { View } from './components/View';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { dataService } from "./services/dataService";
+import { motion, AnimatePresence } from "framer-motion"
+
+function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(dataService);
+  }, [])
+
+  return (
+    <div className="App">
+      {data.map(element => {
+        if (element.subtext) return <View key={element.id} name={element.title} subtext={true} cssPointer={element.cssPointer} />
+        return <div className="border">
+          <motion.div
+            className='motion'
+            initial={{ opacity: 0, }}
+            whileHover={{ opacity: 100, transition: { ease: "easeInOut", duration: 8 }, }}> <View key={element.id} name={element.title} subtext={false} cssPointer={element.cssPointer} />
+          </motion.div>
+        </div>
+      })}
+    </div>
+  );
+}
+
+export default App;
