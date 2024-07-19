1a4bc8e6ceb4a5f82fadafd8b35b3f54a1f39078
Nicolas Corbett <nicolas.corbett@student.ehb.be>
2024-07-19 22:48:38 +0200

    message

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,13 @@ function App() {
       {data.map(element => {
         if (element.subtext) return <View key={element.id} name={element.title} subtext={true} cssPointer={element.cssPointer} />
         return <div className="border">
-          <motion.div
-            className='motion'
-            initial={{ opacity: 0, }}
-            whileHover={{ opacity: 100, transition: { ease: "easeInOut", duration: 8 }, }}> <View key={element.id} name={element.title} subtext={false} cssPointer={element.cssPointer} />
-          </motion.div>
+          <a className='aa' href="/references">
+            <motion.div
+              className='motion'
+              initial={{ opacity: 0, }}
+              whileHover={{ opacity: 100, transition: { ease: "easeInOut", duration: 8 }, }}> <View key={element.id} name={element.title} subtext={false} cssPointer={element.cssPointer} />
+            </motion.div>
+          </a>
         </div>
       })}
     </div>
--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+import Ref from './routes/Ref';
+
+const router = createBrowserRouter([
+  {path: '/', 
+   element: <App /> 
+  },{
+    path: '/references',
+    element: <Ref/>
+  }
+])
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
